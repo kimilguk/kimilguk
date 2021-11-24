9b67a9903e1a03cfbbc7ea231ebbf26f4aa927d9
boramcom <boramcom@daum.net>
2021-11-24 18:48:01 +0900

    2주차 1교시 작업소스

--- app/src/main/java/com/global/kimilguk/MainActivity.java
+++ app/src/main/java/com/global/kimilguk/MainActivity.java
@@ -5,16 +5,27 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    String hello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hello = new String();
+        Button btnAnonymous = findViewById(R.id.btnAnonymous);
+        btnAnonymous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"익명클래스로 버튼에 onClick액션을 실행 했습니다.", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
     public void onClickBtnMethod(View view) {
