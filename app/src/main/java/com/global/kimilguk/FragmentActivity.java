550ed78c23dbe912297bee75a9651e3ab54abab5
boramcom <boramcom@daum.net>
2021-12-09 18:50:41 +0900

    5주차 2교시 작업소스

--- app/src/main/java/com/global/kimilguk/FragmentActivity.java
+++ app/src/main/java/com/global/kimilguk/FragmentActivity.java
@@ -1,10 +1,18 @@
 package com.global.kimilguk;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FragmentActivity extends AppCompatActivity {
@@ -33,5 +41,42 @@ public class FragmentActivity extends AppCompatActivity {
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, fragment2).commit();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //화면에 메뉴 출력=inflate
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        //메뉴 아이템 중에서 검색을 위해 정의한 아이템의 액션 뷰객체 생성
+        View view = menu.findItem(R.id.menu_search).getActionView();
+        EditText editText = view.findViewById(R.id.txtEditSearch);//검색어입력 객체생성
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override //입력상자 리스너객체의 수정이벤트 메소드
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                // 키보드 내리기
+                InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                inputMethodManager.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+                Toast.makeText(getApplicationContext(),"입력된 검색어는: " + editText.getText(), Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int selectedId = item.getItemId();
+        switch(selectedId) {
+            /*case R.id.menu_search:
+                Toast.makeText(this, "검색메뉴가 선택 되었습니다.", Toast.LENGTH_SHORT).show();
+                break;*/
+            case R.id.menu_settings:
+                Toast.makeText(this, "설정메뉴가 선택 되었습니다.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_hidden:
+                Toast.makeText(this, "숨김메뉴가 선택 되었습니다.", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
