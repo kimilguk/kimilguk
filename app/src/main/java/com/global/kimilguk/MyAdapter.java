ae3ad8c1a89c6080d1f5f976fbcddc9ca1357086
boramcom <boramcom@daum.net>
2021-12-22 14:07:07 +0900

    9주차 1교시 작업소스

--- app/src/main/java/com/global/kimilguk/MyAdapter.java
+++ app/src/main/java/com/global/kimilguk/MyAdapter.java
@@ -16,22 +16,22 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     public MyAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());//출력 객체 생성
         View cardView = inflater.inflate(R.layout.card_view, parent, false);//카드 뷰 출력
-        return new MyViewHolder(cardView);
+        return new MyViewHolder(cardView);//인플레이터로 카드뷰를 화면에 출력한다
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyAdapter.MyViewHolder holder, int position) {
-        //바인딩 시킬 값 변경
-        holder.imgViewTel.setImageResource(android.R.drawable.presence_away);
+        //카드 뷰 1  개 position 에 대한 값을 holder에 바인딩 시킬 때 사용
+        /*holder.imgViewTel.setImageResource(android.R.drawable.presence_away);
         holder.txtTelName.setText("홍길동");
-        holder.txtTelDetail.setText("111-1111-1111");
+        holder.txtTelDetail.setText("111-1111-1111");*/
     }
 
     @Override
     public int getItemCount() {
-        return 5;
+        return 5; //출력 할 카드 뷰 갯수 지정
     }
-    //카드 뷰 객체 생성 클래스
+    //카드 뷰 디자인 객체로 사용할 클래스 생성
     public class MyViewHolder extends RecyclerView.ViewHolder {
         ImageView imgViewTel;
         TextView txtTelName;
--- app/src/main/java/com/global/kimilguk/RecyclerViewActivity.java
+++ app/src/main/java/com/global/kimilguk/RecyclerViewActivity.java
@@ -15,8 +15,9 @@ public class RecyclerViewActivity extends AppCompatActivity {
         //객체 생성
         RecyclerView recyclerView = findViewById(R.id.recyclerView1);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        //리사이클러 뷰 객체를 리니어레이아웃 배치로 설정
         recyclerView.setLayoutManager(linearLayoutManager);
-        MyAdapter myAdapter = new MyAdapter();
-        recyclerView.setAdapter(myAdapter);
+        MyAdapter myAdapter = new MyAdapter();//어댑터 객체 생성
+        recyclerView.setAdapter(myAdapter);//리사이클러뷰와 어댑터 연결
     }
 }
