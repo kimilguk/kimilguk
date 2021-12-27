05e19e6812776125966cd4149e8b966dab9e8319
boramcom <boramcom@daum.net>
2021-12-27 12:46:57 +0900

    10주차 2교시 작업

--- app/src/main/java/com/global/kimilguk/ThreadActivity.java
+++ app/src/main/java/com/global/kimilguk/ThreadActivity.java
@@ -9,13 +9,14 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ThreadActivity extends AppCompatActivity {
+    TextView txtThreadValue;//클래스 멤버변수 선언
     BackgroundThread thread;//이너 클래스 생성
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thread);
         //객체 생성
-        TextView txtThreadValue = findViewById(R.id.txtThreadValue);
+        txtThreadValue = findViewById(R.id.txtThreadValue);
         Button btnThreadStart = findViewById(R.id.btnThreadStart);
         btnThreadStart.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +44,8 @@ public class ThreadActivity extends AppCompatActivity {
                     Thread.sleep(1000);
                     value++;//동일 코딩: value = value + 1; value += 1;
                     Log.d("스레드","value 값: "+value);
+                    //스레드 클래스에서 메인스레드의 UI내용 변경 시 에러 발생됨
+                    txtThreadValue.setText("value 값: "+value);
                 } catch (InterruptedException e) {
                     currentThread().interrupt();
                 }
